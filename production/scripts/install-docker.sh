3dd881875d2f4b6cef84a5a5528c4bfbdb24577e
anderson-felix <andersonfelixmsouza@gmail.com>
2022-03-22 23:10:34 -0300

    ci(workflow): update actions

--- production/scripts/install-docker.sh
+++ production/scripts/install-docker.sh
@@ -1,38 +1,41 @@
 #!/usr/bin/bash
 
-# The following instructions were extracted from https://docs.docker.com/engine/install/ubuntu/
+if ! which docker; then
 
-# 0 - prevent interactive mode
-export DEBIAN_FRONTEND=noninteractive
+  # 0 - prevent interactive mode
+  export DEBIAN_FRONTEND=noninteractive
 
-# 1 - update the apt package index
-sudo apt-get update -y
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release -y
+  # 1 - update the apt package index
+  sudo apt-get -y update && sudo apt-get -y upgrade
+  sudo apt-get install \
+      ca-certificates \
+      curl \
+      gnupg \
+      lsb-release -y
 
-# 2 - add docker's official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --batch --yes -o /usr/share/keyrings/docker-archive-keyring.gpg
+  # 2 - add docker's official GPG key
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --batch --yes -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-# 3 - setup a stable repository
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  # 3 - setup a stable repository
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# 4 - install docker
-sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+  # 4 - install docker
+  sudo apt-get update -y
+  sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
-# 5 - create the docker group
-sudo groupadd docker
+  # 5 - create the docker group
+  sudo groupadd docker
 
-# 6 - add the current user permanently to the docker group
-sudo usermod -aG docker $USER
+  # 6 - add the current user permanently to the docker group
+  sudo usermod -aG docker $USER
 
-# 7 - verify the install version
-docker --version
+  # 7 - verify the install version
+  docker --version
 
-# 8 - reboot signal
-export NEED_REBOOT=true
+  # 8 - reboot signal
+  export NEED_REBOOT=true
+
+else
+  echo "Docker already installed"
