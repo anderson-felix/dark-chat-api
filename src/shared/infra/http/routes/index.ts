a1bc759c49f530b498b825e32e6aceae630a9248
Anderson Felix <andersonfelixmsouza@gmail.com>
2023-10-03 17:37:19 -0300

    Updated routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -6,4 +6,9 @@ routes.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
 
+routes.post('/webhook', (req, res) => {
+  console.log('_____TEMPORARY WEBHOOK CALLED\n', JSON.stringify({data: req.body}, null, 4));
+  res.sendStatus(200);
+});
+
 export default routes;
